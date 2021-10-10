7c6b36f18f929f61d8f18dd46c43b734a70b7d3e
krzysiek1421 <krzysiek1421@gmail.com>
2021-10-10 18:13:27 +0200

    I should have done it this way

--- P3/Lab_nr_1.cs
+++ P3/Lab_nr_1.cs
@@ -8,18 +8,15 @@ namespace P3_Lab_nr1
         public string FirstName;
         public string LastName;
         public int Age;
-    }
+        public int Id;
 
+        public override string ToString()
+        {
+            return $"Person ID: {Id} | Name: {FirstName} {LastName} | Age: {Age}";
+        }
+    }
     class Program
     {
-        static void ListAll(Person[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine("Person #" + (i+1) + "| Name: " + array[i].FirstName + "| Last Name: " +
-                                  array[i].LastName + "| Age: " + array[i].Age);
-            }
-        }
         static int GenAge()
         {
             Random rnd = new Random();
@@ -40,14 +37,19 @@ namespace P3_Lab_nr1
         {
             Person[] people = new Person[5];
             var personGenerator = new PersonNameGenerator();
-            for(int i = 0; i < people.Length; i++)
+            for (int i = 0; i < people.Length; i++)
             {
                 people[i] = new Person();
                 people[i].FirstName = personGenerator.GenerateRandomFirstName();
                 people[i].LastName = personGenerator.GenerateRandomLastName();
                 people[i].Age = GenAge();
+                people[i].Id = (i + 1);
             }
-            ListAll(people);
+
+            foreach (var p in people)
+            {
+                Console.WriteLine(p.ToString());
+            }
             SumAge(people);
         }
     }
